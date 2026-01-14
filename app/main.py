2d18e906ecff92dafc2eef397269822466fd107b
ibn-misbah <kawnuma@gmail.com>
2026-01-14 23:13:57 +0300

    feat: main app entry

---
+++ app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+from app.api.v1.api import api_router
+
+# =========== main app instance =============
+app = FastAPI(description="Task api")
+
+
+# ======= Route registery =======
+app.include_router(api_router)
+
+
+# ========== Health status check ========
+@app.get("/health")
+async def health():
+    return {
+        "success": True,
+        "message": "Health check passed",
+    }

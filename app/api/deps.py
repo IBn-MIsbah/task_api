6d547d3afd25f6489cc8a3f0af59dcd99e468342
ibn-misbah <kawnuma@gmail.com>
2026-01-15 11:19:39 +0300

    feat: Dependency to get the current user

---
+++ app/api/deps.py
@@ -0,0 +1,31 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.config import settings
+from app.core.db import get_db
+from app.models.user import User
+
+reusable_oauth2 = OAuth2PasswordBearer(tokenUrl="api/v1/auth/login/access-token")
+
+
+async def get_current_user(
+    session: AsyncSession = Depends(get_db), token: str = Depends(reusable_oauth2)
+) -> User:
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=settings.ALGORITHM)
+        user_id: str = payload.get("sub")
+
+        if user_id is None:
+            raise HTTPException(
+                status_code=403, detail="Could not validate credentials"
+            )
+
+    except JWTError:
+        raise HTTPException(status_code=403, detail="Could not validate creadentials")
+
+    user = await session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user

3bb32d9d639f6c950c459dc4fbe1f006e888e9da
ibn-misbah <kawnuma@gmail.com>
2026-01-15 11:20:17 +0300

    feat: Register authentication route

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.v1 import users
+from app.api.v1 import tasks, users, auth
 
 api_router = APIRouter(redirect_slashes=False, prefix="/api/v1")
 
 api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(tasks.router, prefix="/tasks", tags=["tasks"])
+api_router.include_router(auth.router, prefix='/auth', tags=['auth'])

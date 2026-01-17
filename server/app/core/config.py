d75b2fdff6dbda5426cd8291757205a42500dca8
ibn-misbah <kawnuma@gmail.com>
2026-01-17 17:20:30 +0300

    feat: Distinguish between access and refresh token secret - Added expiry duration for access and refresh token

--- server/app/core/config.py
+++ server/app/core/config.py
@@ -3,9 +3,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DATABASE_URL: str
-    SECRET_KEY: str
+    ACCESS_TOKEN_SECRET_KEY: str
+    REFRESH_TOKEN_SECRET_KEY: str
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: float = 0.5
+    REFRESH_TOKEN_EXPIRE_MINUTES: int = 10
 
     model_config = SettingsConfigDict(env_file=".env")
 

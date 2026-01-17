f765a12987b2237675b086902afde5385f2c5a88
ibn-misbah <kawnuma@gmail.com>
2026-01-17 23:42:41 +0300

    Refactor forma

--- server/app/api/v1/api.py
+++ server/app/api/v1/api.py
@@ -7,4 +7,3 @@ api_router = APIRouter(redirect_slashes=False, prefix="/api/v1")
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(tasks.router, prefix="/tasks", tags=["tasks"])
-
